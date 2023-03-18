2d3d8530f6eb52d9c1733e02a797ebc4fb2da540
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-18 12:35:10 +0530

    Added : Code to Test the Analyse Mood

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -3,9 +3,16 @@ import org.junit.Assert;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenSentence_WhenContainsSad_ShouldPassTest() {
+    public void givenSentence_WhenContainsSad_ShouldReturnSad() {
         MoodAnalyser object = new MoodAnalyser();
-        String mood = object.analyseMood("this is a sad message");
+        String mood = object.analyseMood("I am in sad mood");
         Assert.assertEquals("sad", mood);
+    }
+
+    @Test
+    public void givenSentence_WhenDoNotContainsSad_ShouldReturnHappy() {
+        MoodAnalyser object = new MoodAnalyser();
+        String mood = object.analyseMood("I am in any mood");
+        Assert.assertEquals("happy", mood);
     }
 }
